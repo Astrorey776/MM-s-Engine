972b9fd5525f33c9616c2439d6fb01d87ef3a43e
Astrorey776 <astrorey776@gmail.com>
2022-12-03 17:48:45 +0100

    AABB

--- GameEngine/R_Mesh.cpp
+++ GameEngine/R_Mesh.cpp
@@ -12,7 +12,16 @@ M_Mesh::~M_Mesh()
 	delete[] indices;
 }
 
-void M_Mesh::meshRenderer(mat4x4 globalT, TextureTypes textureT)
+void M_Mesh::InitAABB()
+{
+	for (size_t i = 0; i < num_vertices * VERTEX_ARGUMENTS; i += VERTEX_ARGUMENTS) {
+		tempVert.emplace_back(vertices[i], vertices[i + 1], vertices[i + 2]);
+	}
+	AABB_.SetFrom(&tempVert[0], tempVert.size());
+
+}
+
+void M_Mesh::meshRenderer(float4x4 globalT, TextureTypes textureT, float4x4 global)
 {
 
 	glEnable(GL_TEXTURE_COORD_ARRAY);
@@ -33,7 +42,7 @@ void M_Mesh::meshRenderer(mat4x4 globalT, TextureTypes textureT)
 
 
 	glPushMatrix();
-	glMultMatrixf(&globalT);
+	glMultMatrixf(global.ptr());
 
 	glDrawElements(GL_TRIANGLES, num_indices, GL_UNSIGNED_INT, NULL);
 
@@ -53,6 +62,45 @@ void M_Mesh::meshRenderer(mat4x4 globalT, TextureTypes textureT)
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisable(GL_TEXTURE_2D);
 	glDisable(GL_TEXTURE_COORD_ARRAY);
+
+
+	OBB_ = AABB_;
+	OBB_.Transform(global);
+	global_AABB.SetNegativeInfinity();
+	global_AABB.Enclose(OBB_);
+
+}
+
+
+
+void M_Mesh::RenderAABB() 
+{
+
+	OBB_.GetCornerPoints(tempCorn1);
+	DrawAABB_Boxes(tempCorn1, float3(0, 255, 0));
+
+	global_AABB.GetCornerPoints(tempCorn2);
+	DrawAABB_Boxes(tempCorn2, float3(0, 255, 0));
+
+
+}
+
+void M_Mesh::DrawAABB_Boxes(float3* tempCorn, float3 color)
+{
+
+	int indices[24] = { 0,2,2,6,6,4,4,0,0,1,1,3,3,2,4,5,6,7,5,7,3,7,1,5 };
+
+	glBegin(GL_LINES);
+	glColor3fv(color.ptr());
+
+	for (size_t i = 0; i < 24; i++)
+	{
+		glVertex3fv(tempCorn[indices[i]].ptr());
+	}
+
+	glColor3f(255.f, 255.f, 255.f);
+	glEnd();
+
 }
 
 
