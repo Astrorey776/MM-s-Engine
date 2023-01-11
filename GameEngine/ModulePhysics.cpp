4da67c244026b334ba6a61044de54d57bb5b827f
Astrorey776 <astrorey776@gmail.com>
2023-01-11 08:31:44 +0100

    ModulePhysics

--- GameEngine/ModulePhysics.cpp
+++ GameEngine/ModulePhysics.cpp
@@ -2,14 +2,37 @@
 #include "Globals.h"
 #include "ModulePhysics.h"
 
+#ifdef _DEBUG
+#pragma comment (lib, "External_Libraries/Bullet/libx86/BulletDynamics_debug.lib")
+#pragma comment (lib, "External_Libraries/Bullet/libx86/BulletCollision_debug.lib")
+#pragma comment (lib, "External_Libraries/Bullet/libx86/LinearMath_debug.lib")
+#else
+#pragma comment (lib, "External_Libraries/Bullet/libx86/BulletDynamics.lib")
+#pragma comment (lib, "External_Libraries/Bullet/libx86/BulletCollision.lib")
+#pragma comment (lib, "External_Libraries/Bullet/libx86/LinearMath.lib")
+#endif
 
 ModulePhysics3D::ModulePhysics3D(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
+	debug = false;
+
+	collision_conf = new btDefaultCollisionConfiguration();
+	dispatcher = new btCollisionDispatcher(collision_conf);
+	broad_phase = new btDbvtBroadphase();
+	solver = new btSequentialImpulseConstraintSolver();
+
+	//Error ambiguo
+	//debug_draw = new DebugDrawer();
 }
 
 // Destructor
 ModulePhysics3D::~ModulePhysics3D()
 {
+	delete debug_draw;
+	delete solver;
+	delete broad_phase;
+	delete dispatcher;
+	delete collision_conf;
 }
 
 // Render not available yet----------------------------------
